58c738ac3b1c2c07cde6cf8a4c279d6f79a03e8b
Verity Davis <toastnjamz@users.noreply.github.com>
2018-10-13 20:17:07 -0700

    stack.cs

---
+++ stack.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack theStack = new Stack(4);
+            theStack.push("Star Wars");
+            theStack.push("Jaws");
+            theStack.push("Jaws 2");
+            theStack.push("Titanic");
+            theStack.push("Rocky");
+
+            WriteLine("===================== \nAbout to peek:");
+            WriteLine("'" + theStack.peek() + "'" + " is at the top of the stack.\n");
+
+            WriteLine("===================== \nThe Stack contains:\n");
+            while (!theStack.isEmpty())
+            {
+                string movie = theStack.pop();
+                WriteLine(movie);
+            }
+        }
+    }
+
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+
+        public void push(string m)
+        {
+            if (isFull())
+            {
+                WriteLine("This stack is full.");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = m;
+            }
+        }
+
+        private bool isFull()
+        {
+            return(maxSize - 1 == top);
+            //It's -1 b/c of how we initialize the constructor
+            //maxSize is a value set passed in
+        }
+
+        public string pop()
+            {
+                if (isEmpty())
+                {
+                    WriteLine("The stack is empty.");
+                    return "--";
+                }
+                else
+                {
+                    int old_top = top;
+                    top--;
+                    return stackArray[old_top];
+                }
+            }
+
+        public string peek()
+        {
+            return stackArray[top];
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+    }
+}
