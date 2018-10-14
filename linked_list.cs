8ca126d6986fc17ffbbc6c66140297990c6305a6
Verity Davis <toastnjamz@users.noreply.github.com>
2018-10-13 20:13:16 -0700

    Create linked_list.cs

---
+++ linked_list.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList myList = new SinglyLinkedList();
+            myList.insertFirst(100);
+            myList.insertFirst(50);
+            myList.insertFirst(99);
+            myList.insertFirst(88);
+            myList.insertLast(999000);
+            myList.displayList();
+
+            myList.deleteFirst();
+            myList.displayFirst();
+        }
+    }
+
+    public class SinglyLinkedList
+    {
+        private Node first;
+        public bool isEmpty()
+        {
+            return (first == null);
+        }
+    }
+
+    public class insertFirst(int data)
+    {
+        Node newNode = new Node();
+        newNode.data = Data;
+        newNode.next = first;
+        first = newNode;
+    }
+
+    public Node deleteFirst()
+    {
+        Node temp = first;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList()
+    {
+        Console.WriteLine("List (first -- last) ");
+        Node current = first;
+        while (current != null)
+        {
+            current.displayNode();
+            current = current.next;
+        }
+
+        Console.WriteLine();
+    }
+
+    public void insertLast(int data);
+    {
+        Node current = first;
+        while(current.next =! null)
+        {
+            current = current.next;
+        }
+
+        Node newNode = new Node();
+        newNode.data = data;
+        current.next = newNode;
+    }
+
+    public class Node 
+    {
+        public int data;
+        public Node next;
+        public void displayNde()
+        {
+            Console.WriteLine("<" + data + ">");
+        }
+    }
+}
